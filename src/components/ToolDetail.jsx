eed7e196196f62959be5bde90b180a6a34469e4d
Ayhan Durak <ayhandurak@Ayhans-MacBook-Pro.local>
2020-10-22 21:32:58 -0700

    adding tool detail component

--- src/components/ToolDetail.jsx
+++ src/components/ToolDetail.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {navigate} from '@reach/router'
+import axios from 'axios'
 
-const ToolDetail = () =>{
+const ToolDetail = (props) =>{
+    const [tool, setTool] = useState({})
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/tools/${props.id}`)
+        .then(res => setTool(res.data))
+        .catch(err =>  console.log(err))
+    },[])
+    const deleteTool = () => {
+        axios.delete(`http://localhost:8000/api/tools/${props.id}`)
+        .then(res => navigate("/"))
+        .catch(err => console.log(err))
+    }
     return(
-        <div className="container mt-3">
-            <div className="row">
-                <div className="col">
-                    <h1>This is tool detail page</h1>
-                </div>
-            </div>
-        </div>
+        <div className="container">
+        <h1>{tool.name}</h1>
+    <div className="card text-center">
+    <div className="card-header font-weight-bold">{tool.name}</div>
+    <div className="card-body">
+    <dl className="row card-text">
+        <dt className="col-sm">Photo:</dt>
+        <dd className="col-sm">{tool.photo}</dd>
+    </dl>
+    <dl className="row card-text">
+        <dt className="col-sm">Tool Price :</dt>
+        <dd className="col-sm">{tool.price}</dd>
+    </dl>
+    <dl className="row card-text">
+        <dt className="col-sm">Description :</dt>
+        <dd className="col-sm">{tool.description}</dd>
+    </dl>
+        <button className="btn btn-danger m-1" onClick={deleteTool}>Save</button>
+        <button className="btn btn-secondary m-1" onClick={() => navigate("/")}>Back to Home</button>
+    </div>
+    </div>
+    </div>
         
     )
 }
