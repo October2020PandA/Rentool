0b0637c49f6eb23068b8c2b3bbfd56ecd3c471be
Eric Mendez <ericyoary@gmail.com>
2020-10-23 17:44:35 -0700

    Tool List page functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,25 +12,22 @@ const Login = (props) => {
 
     const registerUser = async (data) => {
         const response = await axios.post("http://localhost:8080/api/register", data);
-        // console.log(response.data);
+        console.log(response.data);
         setUser(response.data)
-        // console.log(user);
     }
-
+    
     const login = async (data) => {
-        const response = await axios.post("http://localhost:8080/api/login", data);
+        const response = await axios.post("http://localhost:8080/api/login", {email: data.loginEmail, password: data.loginPassword});
         if (response.data) {
             console.log("login: ", response.data);
             setUser(response.data)
             navigate(`/dashboard/new`)
-
-            // console.log(user);
         } else {
             //TODO: Tell user does not exist
             // console.log("errrrrorrr");
         }
     }
-
+    
     return (
         <div className="container">
             <ul className="nav justify-content-end">
@@ -39,56 +36,59 @@ const Login = (props) => {
                 </li>
             </ul>
             <h1 className="font-weight-bold">RenTool</h1>
-            <hr/>
+            <hr />
             <div className="login-form">
                 <h2 className="text-start">User Login</h2>
             </div>
-            
-                {
-                    viewLogin
-                        ?
-
-                        <FormGroup onSubmit={handleSubmit(login)} className="colum">
+    
+            {
+                viewLogin
+                    ?
+                    <form onSubmit={handleSubmit(login)}>
+                        <FormGroup className="colum">
                             <FormGroup>
-                                <i class="fas fa-user"></i>
-                                <input type="text" placeholder="Username" name="username" ref={register} />
+                                <i className="fas fa-user"></i>
+                                <input type="text" placeholder="Email" name="loginEmail" ref={register} />
                             </FormGroup>
-
+    
                             <FormGroup>
-                                <i class="fas fa-lock"></i>
-                                <input type="password" placeholder="Password" name="password" ref={register} />
+                                <i className="fas fa-lock"></i>
+                                <input type="password" placeholder="Password" name="loginPassword" ref={register} />
                             </FormGroup>
-                            
+    
                             <button className="btn-lg btn-dark btn-block">Login</button>
                             <span className="p-2" onClick={() => setViewLogin(prevState => !prevState)}>No Account? Register</span>
                         </FormGroup>
-                        :
-                        <FormGroup onSubmit={handleSubmit(registerUser)} className="colum">
+                    </form>
+                    :
+                    <form onSubmit={handleSubmit(registerUser)}>
+                        <FormGroup className="colum">
                             <FormGroup>
-                            <input type="text" placeholder="Name" name="name" ref={register} />
+                                <input type="text" placeholder="Name" name="name" ref={register} />
                             </FormGroup>
-
+    
                             <FormGroup>
-                            <input type="email" placeholder="Email" name="email" ref={register} />
+                                <input type="email" placeholder="Email" name="email" ref={register} />
                             </FormGroup>
-
+    
                             <FormGroup>
-                            <input name="password" placeholder="Password" type="password"  ref={register} />
+                                <input name="password" placeholder="Password" type="password" ref={register} />
                             </FormGroup>
-
+    
                             <FormGroup>
-                            <input type="text" placeholder="Zip" name="zip" ref={register} />
+                                <input type="text" placeholder="Zip" name="zip" ref={register} />
                             </FormGroup>
-
+    
                             <FormGroup>
-                            <input type="text" placeholder="State" name="state" ref={register} />
+                                <input type="text" placeholder="State" name="state" ref={register} />
                             </FormGroup>
-
+    
                             <button className="btn-lg btn-dark btn-block">Register</button>
                             <span className="p-2" onClick={() => setViewLogin(prevState => !prevState)}>Have an account? Login</span>
                         </FormGroup>
-                }
-            
+                    </form>
+            }
+    
             <div className="terms-of-service">
                 <span>Terms of Service | </span>
                 <span>Contact</span>
--- src/components/ToolList.jsx
+++ src/components/ToolList.jsx
@@ -1,57 +1,93 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useContext} from 'react'
 import {Link, navigate} from '@reach/router'
 import axios from 'axios'
+import {UserContext} from '../provider/Provider'
 
 const ToolList = () => {
     const [tools, setTools] = useState([]);
     const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState('')
     const [page, setPage] = useState(1)
+    const [user, setUser] = useContext(UserContext);
+    const [totalPages, setTotalPages] = useState([{}])
 
     useEffect(() => {
         const getTools = async () => {
             const response = await axios.get(`http://localhost:8080/api/tools/page/${page}`)
             console.log(response.data.content);
+            console.log(response.data)
             setTools(response.data.content)
+            let arr = []
+            for (let i = 0; i < response.data.totalPages; i++) {
+                arr.push({})
+            }
+            setTotalPages(arr)
         }
         getTools()
     },[page])
+
+    const postItemClick = () => {
+        if (user.id) {
+            navigate('/dashboard/new')
+        } else {
+            navigate('/signin')
+        }
+    }
+
+    const getNewPage = async (pageNumber) => {
+        if (pageNumber === page) return
+        const response = await axios.get(`http://localhost:8080/api/tools/page/${pageNumber}`)
+        setTools(response.data.content)
+        setPage(pageNumber)
+    }
 
     return(
         <div className="container mt-3">
             <ul className="nav justify-content-end">
                 <li className="nav-item">
-                    <button className="btn btn-secondary btn-m" onClick={() => navigate(`/signin`)}>Post Item</button>
+                    <button className="btn btn-secondary btn-m" onClick={postItemClick}>Post Item</button>
                 </li>
             </ul>
-
+    
             <h1 className="font-weight-bold">RenTool</h1>
                   
             <div className="search">
-			<label className="search-label" htmlFor="search-input">
-				<input
-					type="text"
-					name="query"
-					id="search-input"
+            <label className="search-label" htmlFor="search-input">
+                <input
+                    type="text"
+                    name="query"
+                    id="search-input"
                     placeholder="Search..."
                     onChange={ e => setSearch(e.target.value) }
-				/>
-				<i className="fa fa-search search-icon" aria-hidden="true"/>
-			</label>
+                />
+                <i className="fa fa-search search-icon" aria-hidden="true"/>
+            </label>
                 <div className="col">
-
+    
                 </div>
             </div>
             <hr/>
             <div>
                 {
                     tools.map(tool => (
-                        <div key={tool.id}>
-                            {tool.name}
+                        <div className="" key={tool.id} onClick={() => navigate(`/dashboard/${tool.id}`)}>
+                            <img src={tool.image} alt="tool"/>
+                            <span>{tool.name}</span>
+                            <span>${tool.price}</span>
                         </div>
                     ))
                 }
             </div>
+            <div>
+                {
+                    totalPages.map((page, i) => (
+                        <span key={i} onClick={() => getNewPage(i + 1)}>{i + 1}</span>
+                    ))
+                }
+                <span>Viewing Page: {page}</span>
+            </div>
+            
+            
         </div>
 
     )
